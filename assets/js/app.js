6d800af54317ff93ff5bae1609d9f8cbaa86791f
Harsh Vaishnav <harshvaishnav2396@gmail.com>
2023-12-26 16:34:14 +0530

    first

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+let cl = console.log;
+
+// let numArr = [23, 444, 54, 22, 33, 55]
+
+// function maxminNum(nums){
+//     let max = 0;
+//     let min = 0;
+
+//     nums.forEach(ele => {
+//         if(ele > max){
+//             max = ele
+//         }
+//         if(ele < min){
+//             min = ele
+//         }
+//     });
+//     // return {max : max, min : min}
+//     return `maxNum = ${max} and minNum = ${min}`
+// }
+// cl(maxminNum(numArr))
+
+
